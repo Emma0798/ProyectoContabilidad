09f5c74fe8e8dadcd21e0c0628d9a386f77866f3
AgustinVillavicencio <villavicencio.agustin.17@gmail.com>
2023-11-13 08:45:29 -0300

    Actualizacion de bd y comienzo de requisito 2

--- contabilidad.sql
+++ contabilidad.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 12-11-2023 a las 23:19:45
+-- Tiempo de generación: 13-11-2023 a las 11:44:04
 -- Versión del servidor: 8.0.31
 -- Versión de PHP: 8.0.26
 
@@ -39,6 +39,34 @@ CREATE TABLE IF NOT EXISTS `libro_diario` (
   KEY `FK_mayor` (`FK_mayor`),
   KEY `FK_plan_de_cuentas` (`FK_plan_de_cuentas`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `libro_diario`
+--
+
+INSERT INTO `libro_diario` (`nroAsiento`, `fecha`, `debe`, `haber`, `FK_mayor`, `FK_plan_de_cuentas`) VALUES
+(1, '2023-01-01', 100, 0, 1, 1001),
+(2, '2023-01-01', 50, 0, 2, 1002),
+(3, '2023-01-01', 100, 0, 3, 1004),
+(4, '2023-01-01', 0, 250, 4, 3001),
+(5, '2023-01-02', 150, 0, 3, 1004),
+(6, '2023-01-02', 0, 100, 1, 1001),
+(7, '2023-01-02', 0, 50, 2, 1002),
+(8, '2023-01-03', 200, 0, 1, 1001),
+(9, '2023-01-03', 0, 200, 5, 4001),
+(10, '2023-01-03', 100, 0, 6, 5002),
+(11, '2023-01-03', 0, 100, 3, 1004),
+(12, '2023-01-04', 1000, 0, 2, 1002),
+(13, '2023-01-04', 0, 1000, 7, 2002),
+(14, '2023-01-05', 1000, 0, 1, 1001),
+(15, '2023-01-05', 0, 1000, 7, 2002),
+(16, '2023-01-06', 1500, 0, 8, 1003),
+(17, '2023-01-06', 0, 1000, 2, 1002),
+(18, '2023-01-06', 0, 500, 1, 1001),
+(19, '2023-01-07', 100, 0, 1, 1001),
+(20, '2023-01-07', 0, 100, 5, 4001),
+(21, '2023-01-07', 50, 0, 6, 5002),
+(22, '2023-01-07', 0, 50, 3, 1004);
 
 -- --------------------------------------------------------
 
@@ -79,16 +107,14 @@ CREATE TABLE IF NOT EXISTS `plan_de_cuentas` (
 INSERT INTO `plan_de_cuentas` (`rubro`, `nroCuenta`, `descripcion`) VALUES
 ('A', 1001, 'Caja'),
 ('A', 1002, 'Bancos'),
-('A', 1003, 'Cuentas por Cobrar'),
-('A', 1004, 'Inventario'),
+('A', 1003, 'Rodados'),
+('A', 1004, 'Mercaderia'),
 ('P', 2001, 'Cuentas por Pagar'),
 ('P', 2002, 'Préstamos'),
 ('PN', 3001, 'Capital Social'),
 ('I', 4001, 'Ventas'),
 ('E', 5001, 'Sueldos'),
-('E', 5002, 'Suministros'),
-('E', 1111111, 'ADQDQ'),
-('A', 435, 'saads');
+('E', 5002, 'Costo de Mercaderia');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
